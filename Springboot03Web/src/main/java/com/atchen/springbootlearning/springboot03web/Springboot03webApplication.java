6e410dea97c39c0ad0e08ceae4a16343c4f36f7d
ChenPeng <854810360@qq.com>
2019-10-18 12:26:59 +0800

    update

--- Springboot03Web/src/main/java/com/atchen/springbootlearning/springboot03web/Springboot03webApplication.java
+++ Springboot03Web/src/main/java/com/atchen/springbootlearning/springboot03web/Springboot03webApplication.java
@@ -2,6 +2,11 @@ package com.atchen.springbootlearning.springboot03web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.View;
+import org.springframework.web.servlet.ViewResolver;
+
+import java.util.Locale;
 
 @SpringBootApplication
 public class Springboot03webApplication {
@@ -9,5 +14,6 @@ public class Springboot03webApplication {
     public static void main(String[] args) {
         SpringApplication.run(Springboot03webApplication.class, args);
     }
+
 
 }
